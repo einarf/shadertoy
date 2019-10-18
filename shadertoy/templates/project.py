4714c57077c90f4025bd0f31a922908f056d1bb0
einarf <eforselv@gmail.com>
2019-10-19 01:10:48 +0200

    Add basic project template

---
+++ shadertoy/templates/project.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from shadertoy import ShaderToyWindow
+
+
+class Project(ShaderToyWindow):
+    resource_dir = Path(__file__).parent
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wnd.position = 0, 0
+
+
+if __name__ == '__main__':
+    Project.run()
